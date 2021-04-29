626f3e8f1b55dadb2de1be5768232c7ececbdf49
Julian Tochman-Szewc <julian.tochmanszewc@gmail.com>
2021-04-29 19:49:03 +0200

    Added react-bootstrap and vanilla bootstrap, tried it out on home page

--- my-app/src/components/navBar.tsx
+++ my-app/src/components/navBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
 import { Page } from "../types/index";
 import { NavigationService } from "../lib/navigationService";
 
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -2,7 +2,51 @@ import React from "react";
 
 const Home: React.FC = () => {
 
-    return (<div></div>);
+    return (
+        <div className="Home">
+            <div className="container">
+                <h1>MirrorMe</h1>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="spinner-border text-dark"/>
+                    </div>
+                    <div className="col-sm-4">
+
+                        <div className="spinner-border text-danger"/>
+
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="spinner-border text-warning"/>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        Link
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        Link
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        Link
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="/">
+                        Disabled
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
