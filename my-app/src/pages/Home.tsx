96bada384c4a2db275bfe437b7dfa8881ee59a81
Julian Tochman-Szewc <julian.tochmanszewc@gmail.com>
2021-05-02 15:28:45 +0200

    Added content

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
+
+
+type gitHubRepo = {
+    id: number;
+    name: string;
+    html_url: string;
+};
+
+
+const getJuliansGitHubRepo = (): Promise<any> => {
+    return axios("https://api.github.com/users/juliants/repos").then((res) => {
+        if (res.status !== 200) throw new Error("Request failed.");
+        return res.data;
+    });
+};
+
 
 const Home: React.FC = () => {
 
+    const [gitRepos, setGitRepos] = useState<gitHubRepo[]>([]);
+    const [bool, setBool] = useState<boolean>(false);
+
+    useEffect(() => {
+        getJuliansGitHubRepo()
+            .then((result) => {
+                setGitRepos(result);
+            })
+            .catch(console.error);
+    }, []);
+
+    const handleClick = () => {
+        setBool(!bool)
+
+    }
+
     return (
+
+
         <div className="Home">
-            <div className="container">
+            <Container fluid>
                 <h1>MirrorMe</h1>
-                <div className="row">
-                    <div className="col-sm-4">
-                        <div className="spinner-border text-dark"/>
-                    </div>
-                    <div className="col-sm-4">
-
-                        <div className="spinner-border text-danger"/>
-
-                    </div>
-                    <div className="col-sm-4">
-                        <div className="spinner-border text-warning"/>
-                    </div>
-                </div>
-            </div>
-
+                <Button onClick={handleClick}>Show Julian's public Git Repos</Button>
+                {bool &&
+                <Row>
+                    {gitRepos.map(x =>
+                        <Col>
+                            <Card style={{ margin: '20px', width: '18rem'}}>
+                                <Card.Body>
+                                    <Card.Title>{x.name}</Card.Title>
+                                    <Card.Text>
+                                        ID: {x.id}
+                                    </Card.Text>
+                                    <Card.Footer><a href={x.html_url}> {x.html_url}</a></Card.Footer>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>}
+            </Container>
 
 
             <ul className="nav">
@@ -45,8 +84,12 @@ const Home: React.FC = () => {
                     </a>
                 </li>
             </ul>
+
         </div>
+
+
     );
 };
+
 
 export default Home;
