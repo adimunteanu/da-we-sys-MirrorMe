a4f4e50cc8ebc377d45a66ff4e897302d177d945
Julian Tochman-Szewc <julian.tochmanszewc@gmail.com>
2021-05-02 19:42:31 +0200

    Added some quality content

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {Button, Card, Col, Container, Row} from "react-bootstrap";
-
+import {Accordion, Button, Card, Col, Container, Nav, Row} from "react-bootstrap";
+import "./Home.css"
 
 type gitHubRepo = {
-    id: number;
-    name: string;
-    html_url: string;
+    id: number,
+    name: string,
+    html_url: string,
 };
+type reddit = {
+    data: {
+        children: [{
+            data: {
+                title: string,
+                secure_media: {},
+                url_overridden_by_dest: string,
 
+            }
+        }]
+    }
+};
 
 const getJuliansGitHubRepo = (): Promise<any> => {
     return axios("https://api.github.com/users/juliants/repos").then((res) => {
@@ -17,11 +28,19 @@ const getJuliansGitHubRepo = (): Promise<any> => {
     });
 };
 
+const getTopRedditMemes = (): Promise<any> => {
+    return axios("https://www.reddit.com/r/memes/top/.json?count=20").then((res) => {
+        if (res.status !== 200) throw new Error("Request failed.");
+        return res.data;
+    });
+};
 
 const Home: React.FC = () => {
 
     const [gitRepos, setGitRepos] = useState<gitHubRepo[]>([]);
-    const [bool, setBool] = useState<boolean>(false);
+    const [boolGit, setBoolGit] = useState<boolean>(false);
+
+    const [memes, setMemes] = useState<reddit>();
 
     useEffect(() => {
         getJuliansGitHubRepo()
@@ -31,23 +50,30 @@ const Home: React.FC = () => {
             .catch(console.error);
     }, []);
 
-    const handleClick = () => {
-        setBool(!bool)
+    useEffect(() => {
+        getTopRedditMemes()
+            .then((result) => {
+                setMemes(result);
+            })
+            .catch(console.error);
+    }, []);
 
+    const handleClickGit = () => {
+        setBoolGit(!boolGit)
     }
 
     return (
 
-
         <div className="Home">
-            <Container fluid>
+            <Container style={{alignItems: "center"}}>
                 <h1>MirrorMe</h1>
-                <Button onClick={handleClick}>Show Julian's public Git Repos</Button>
-                {bool &&
+                <Button onClick={handleClickGit}>Show Julian's public Git Repos</Button>
+
+                {boolGit &&
                 <Row>
                     {gitRepos.map(x =>
                         <Col>
-                            <Card style={{ margin: '20px', width: '18rem'}}>
+                            <Card style={{margin: '20px', width: '18rem'}}>
                                 <Card.Body>
                                     <Card.Title>{x.name}</Card.Title>
                                     <Card.Text>
@@ -58,24 +84,49 @@ const Home: React.FC = () => {
                             </Card>
                         </Col>
                     )}
-                </Row>}
+                </Row>
+                }
+
+                <Accordion>
+                    <h2 style={{marginTop: "30px"}}>Here are some Memes for you</h2>
+
+                    {memes?.data.children.map(y => y.data.secure_media == null &&  //filters out videos from what I know
+                      <div id="memeDiv" style={{margin: "10px"}}>
+                        <Card id="memeCard">
+                          <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey={y.data.title}>
+                                {y.data.title}
+                            </Accordion.Toggle>
+                          </Card.Header>
+                          <Accordion.Collapse eventKey={y.data.title}>
+                            <Card.Body>
+                              <img id="image"
+                                   src={y.data.url_overridden_by_dest}
+                                   alt="seems to be a gif"
+                              />
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                      </div>)
+                    }
+                </Accordion>
+
             </Container>
 
-
-            <ul className="nav">
+            <Nav id="nav">
                 <li className="nav-item">
                     <a className="nav-link" href="/">
-                        Link
+                        Empty Link
                     </a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/">
-                        Link
+                        Empty Link
                     </a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/">
-                        Link
+                        Empty Link
                     </a>
                 </li>
                 <li className="nav-item">
@@ -83,13 +134,11 @@ const Home: React.FC = () => {
                         Disabled
                     </a>
                 </li>
-            </ul>
+            </Nav>
 
         </div>
 
-
     );
 };
-
 
 export default Home;
