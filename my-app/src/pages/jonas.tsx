11bd57c202ec7fa860f0265f04f5d18d27f397d4
Jonas Thierfeldt <jonasthierfeldt@Jonas-Air-2.fritz.box>
2021-05-02 15:44:42 +0200

    added weather status

--- my-app/src/pages/jonas.tsx
+++ my-app/src/pages/jonas.tsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './jonas.css';
 import Carousel from "react-bootstrap/Carousel";
 import img1 from "../img/img1.jpg";
 import img2 from "../img/img2.png";
 import img3 from "../img/img3.jpg";
+import axios from "axios";
+import {Button, Card} from "react-bootstrap";
+
+
+type weatherData = {
+    temperature: string,
+    wind: string,
+    description: string,
+}
+
+const getWeather = (): Promise<any> => {
+    return axios("https://goweather.herokuapp.com/weather/Berlin").then((res) => {
+        if (res.status !== 200) throw new Error("Request failed.");
+        return res.data;
+    });
+};
+
+
+
+
 
 
 const Jonas: React.FC = () => {
 
+    const [data, setData] = useState<weatherData>({temperature: "", wind: "", description: ""});
+    const [bool, setBool] = useState<boolean>(false);
+
+    useEffect(() => {
+        getWeather()
+            .then((result) => {
+                setData(result);
+            })
+            .catch(console.error);
+    }, []);
+
+    const handleClick = () =>  {
+        setBool(!bool);
+    }
+  
+
     return (
+
+       
     <div className="jonas">
+        <Button id="weatherButton" onClick={handleClick}> Show weather of Berlin
+        </Button>
+        {bool && <Card id="weather"> 
+                    <Card.Body>
+                        <Card.Title>
+                            Weather of Berlin
+                        </Card.Title>
+                        <Card.Subtitle>
+                            Today
+                        </Card.Subtitle>
+                        <Card.Text>
+                            <p></p>
+                            <p>Temperature: {data.temperature} </p>
+                            <p>Wind:{data.wind}</p>
+                            <p>Description:{data.description}</p>   
+                        </Card.Text>
+                    </Card.Body>
+                </Card>}
+        
         <h1>Welcome to our Webapplication!</h1>
         <h2>We present: MirrorMe</h2>
         <p>Request your personal data from any Company and we create your Mirror Profile!</p>
@@ -48,4 +105,5 @@ const Jonas: React.FC = () => {
 };
 
 export default Jonas;
+
 
