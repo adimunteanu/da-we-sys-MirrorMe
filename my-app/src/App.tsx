3a3f74474c6b9a0aedd911ddfcc0fe2036514b1a
odedro987 <odedro987@gmail.com>
2021-05-02 01:06:28 -0700

    Added content

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,55 +5,55 @@ import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Adrian from "./pages/adrian";
 import Jonas from "./pages/jonas";
-import Oded from "./pages/oded";
+import OdedTab from "./pages/OdedTab";
 import Tia from "./pages/tia";
 
 
 
 const App: React.FC = () => {
   return (
-      <BrowserRouter>
-        <div className="App">
-          <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-            <ul className="navbar-nav">
-              <li>
-                <Link to="/" className="nav-link">
-                  Home
+    <BrowserRouter>
+      <div className="App">
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul className="navbar-nav">
+            <li>
+              <Link to="/" className="nav-link">
+                Home
                 </Link>
-              </li>
-              <li>
-                <Link to="/Adrian" className="nav-link">
-                  Adrian
+            </li>
+            <li>
+              <Link to="/Adrian" className="nav-link">
+                Adrian
                 </Link>
-              </li>
-              <li>
-                <Link to="/Jonas" className="nav-link">
-                  Jonas
+            </li>
+            <li>
+              <Link to="/Jonas" className="nav-link">
+                Jonas
                 </Link>
-              </li>
-              <li>
-                <Link to="/Oded" className="nav-link">
-                  Oded
+            </li>
+            <li>
+              <Link to="/Oded" className="nav-link">
+                Oded
                 </Link>
-              </li>
-              <li>
-                <Link to="/Tia" className="nav-link">
-                  Tia
+            </li>
+            <li>
+              <Link to="/Tia" className="nav-link">
+                Tia
                 </Link>
-              </li>
-            </ul>
-          </nav>
+            </li>
+          </ul>
+        </nav>
 
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/Adrian" component={Adrian} />
-            <Route path="/Jonas" component={Jonas} />
-            <Route path="/Oded" component={Oded} />
-            <Route path="/Tia" component={Tia} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Adrian" component={Adrian} />
+          <Route path="/Jonas" component={Jonas} />
+          <Route path="/Oded" component={OdedTab} />
+          <Route path="/Tia" component={Tia} />
 
-          </Switch>
-        </div>
-      </BrowserRouter>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
