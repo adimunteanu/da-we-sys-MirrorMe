b711923fe93fa721ec3a5fefd16863a96c6c3ecc
odedro987 <odedro987@gmail.com>
2021-05-11 09:43:27 -0700

    Added RegisterPage component

---
+++ mirror-me-electron/src/pages/UserController/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import './RegisterPage.css';
+
+const RegisterPage = () => {
+  const history = useHistory();
+
+  const register = () => {
+    console.log('Created a user');
+  };
+
+  const routeToLogin = () => {
+    console.log('Routed to login');
+    history.push('/');
+  };
+
+  return (
+    <div className="RegisterPage">
+      <Card className="RegisterPage-Card">
+        <Card.Body>
+          <Form className="RegisterPage-Form">
+            <Form.Group
+              className="RegisterPage-Form-Group"
+              controlId="formBasicEmail"
+            >
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group
+              className="RegisterPage-Form-Group"
+              controlId="formBasicPassword"
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group
+              className="RegisterPage-Form-Group"
+              controlId="formBasicPassword"
+            >
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button
+              className="RegisterPage-Form-Button"
+              variant="primary"
+              onClick={register}
+            >
+              Submit
+            </Button>
+          </Form>
+          <Button variant="link" onClick={routeToLogin}>
+            Already a member?
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default RegisterPage;
