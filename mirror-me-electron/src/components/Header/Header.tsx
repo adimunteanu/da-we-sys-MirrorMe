c7fd98a0bf4a02c33b6fbfd14d99225a4f368c36
Adrian Munteanu <adimunteanu13@gmail.com>
2021-05-16 19:18:50 +0200

    feat: add empty view of data overview

--- mirror-me-electron/src/components/Header/Header.tsx
+++ mirror-me-electron/src/components/Header/Header.tsx
@@ -1,22 +1,29 @@
 import {
+  IonButton,
   IonButtons,
   IonHeader,
+  IonIcon,
   IonMenuButton,
   IonTitle,
   IonToolbar,
+  useIonModal,
 } from '@ionic/react';
+import { add } from 'ionicons/icons';
 import { useLocation } from 'react-router-dom';
 import React, { FunctionComponent, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Menu from '../Menu/Menu';
 import { changeTitle } from './HeaderSlice';
 import { GlobalState } from '../../types';
+import DataUploadModal from '../../pages/OverviewPage/DataUploadModal/DataUploadModal';
 
 const Header: FunctionComponent = () => {
   const location = useLocation();
   const dispatch = useDispatch();
   const title = useSelector((state: GlobalState) => state.header.title);
-
+  const [present, dismiss] = useIonModal(
+    <DataUploadModal onDismiss={() => dismiss()} />
+  );
   useEffect(() => {
     dispatch(changeTitle(location.pathname));
   }, [dispatch, location]);
@@ -30,6 +37,17 @@ const Header: FunctionComponent = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>{title}</IonTitle>
+          {title === 'Data Overview' && (
+            <IonButtons slot="end">
+              <IonButton
+                fill="clear"
+                onClick={() => present({ cssClass: 'DataUploadModal' })}
+                className="Header-Button"
+              >
+                <IonIcon size="large" icon={add} slot="icon-only" />
+              </IonButton>
+            </IonButtons>
+          )}
         </IonToolbar>
       </IonHeader>
     </div>
