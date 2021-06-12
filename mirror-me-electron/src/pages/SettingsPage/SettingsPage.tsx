9c3db080b02e705e970fa9409af752c299d9b44c
Julian Tochman-Szewc <julian.tochmanszewc@gmail.com>
2021-06-12 23:06:31 +0200

    merged Settingspage

--- mirror-me-electron/src/pages/SettingsPage/SettingsPage.tsx
+++ mirror-me-electron/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,7 +1,89 @@
 import React from 'react';
+import './SettingsPage.scss';
+import {
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectAuthToken,
+  selectNickname,
+} from '../UserController/userControllerSlice';
+import { deleteScoreThunk } from '../ScoreboardPage/scoreControllerSlice';
 
 const SettingsPage: React.FC = () => {
-  return <div>This is the Settings Page</div>;
+  const dispatch = useDispatch();
+
+  const nickname = useSelector(selectNickname);
+  const authToken = useSelector(selectAuthToken);
+
+  const tryDeleteScore = () => {
+    dispatch(
+      deleteScoreThunk({
+        nickname,
+        authToken,
+      })
+    );
+  };
+
+  return (
+    <IonContent className="SettingsPage">
+      <p>Settings page</p>
+      <IonGrid className="CardGrid">
+        <IonRow>
+          <IonCol>
+            <IonCard className="SettingsPage_Card">
+              <IonCardHeader>
+                <IonCardTitle>Change Nickname</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                In order to change your nickname, click here!
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonButton id="change-nickname" size="large">
+                        Coming Soon
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+          <IonCol>
+            <IonCard className="SettingsPage_Card">
+              <IonCardHeader>
+                <IonCardTitle>Delete Score</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                In order to change your nickname, click here!
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonButton
+                        id="delete-score"
+                        size="large"
+                        onClick={tryDeleteScore}
+                      >
+                        Delete Score
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
+  );
 };
 
 export default SettingsPage;
